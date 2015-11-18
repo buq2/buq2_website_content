da79feb010a5438f36dcf049d3a063a9745d3a5c
Matti Jukola <buq2.open.source@gmail.com>
2015-11-18 19:19:46 +0200

    Add kamalaskuri article

---
+++ articles/kamalaskuri.md
@@ -0,0 +1,178 @@
+{
+    "Title": "Kamalaskuri",
+    "LongTitle": "Used skydiving rig value calculator",
+    "Description": "This calculator can be used to calculate value of used skydiving rig in Finnish market",
+    "DateCreated": "2015-11-18 18:17",
+    "DateModified": "2015-11-18 18:17",
+    "Icon": "/content_static/articles/kamalaskuri/icon.png",
+    "Tags": ["skydiving"],
+    "CreateToc": false,
+    "HeadAfterScripts": "<script src='/content_static/articles/kamalaskuri/kamalaskuri.js'></script>"
+}
+
+---------- META END ----------
+
+# v1.0
+
+Laskurin on tarkoitus olla objektiivinen ja tunteeton kamojen arvon
+laskuri joka ottaa huomioon vain käyttöiän, tarvittavat huollot sekä
+yksinkertaisen kuluman.
+
+Kommentteja voi jättää. Korjailen laskuria kommenttien perusteella
+yhteisön mielestä oikeaan suuntaan.
+
+## Laskurin "totuuksia"
+
+Päävarjon arvo uutena on sen ostohinta.
+
+Päävarjon arvo, huollosta ja punoksista huolimatta 20v vanhana on 0€.
+
+Cypressin arvo 12v tai sitä vanhempana on 100€
+
+## Seurauksia:
+
+Punosten vaihtaminen 400€:llä kymmenen vuotta vanhaan päävarjoon nostaa päävarjon arvoa vain 200€:llä.
+
+Vanhojen kamojen huollattaminen nostaa kamojen arvoa vähemmän kuin uusien.
+
+
+
+
+<table style="margin:0 auto;">
+    <tr>
+        <th></th>
+        <th>Valjaat</th>
+        <th>Päävarjo</th>
+        <th>Varavarjo</th>
+        <th>Cypress</th>
+        <th>Yhteensä</th>
+    </tr>
+    <tr>
+        <td>Hinta: </td>
+        <td>
+            <input type="number" id="container-price" name="container-price" value="2600"/>
+        </td>
+        <td>
+            <input type="number" id="main-price" name="main-price" value="2000"/>
+        </td>
+        <td>
+            <input type="number" id="reserve-price" name="reserve-price" value="1200"/>
+        </td>
+        <td>
+            <input type="number" id="aad-price" name="aad-price" value="1200"/>
+        </td>
+        <td>
+            <input type="number" id="total-price" name="total-price" value="0"/>
+        </td>
+    </tr>
+
+    <tr>
+        <td>Valmistusajankohta:</td>
+        <td>
+            <input type="date" id="container-dom" name="container-dom"/>
+        </td>
+        <td>
+            <input type="date" id="main-dom" name="main-dom"/>
+        </td>
+        <td>
+            <input type="date" id="reserve-dom" name="reserve-dom"/>
+        </td>
+        <td>
+            <input type="date" id="aad-dom" name="aad-dom"/>
+        </td>
+    </tr>
+
+    <tr>
+        <td>Seuraava huolto/tarkastus:</td>
+        <td>
+            <input type="date" id="container-next-maint" name="container-next-maint"/>
+        </td>
+        <td>
+            <input type="date" id="main-next-maint" name="main-next-maint" />
+        </td>
+        <td>
+            <input type="date" id="reserve-next-maint" name="reserve-next-maint"/>
+        </td>
+        <td>
+            <input type="date" id="aad-next-maint" name="aad-next-maint" />
+        </td>
+    </tr>
+
+    <tr>
+        <td>Huollon/tarkastuksen hinta:</td>
+        <td>
+            <input type="number" id="container-maint-price" name="container-maint-price" value="80"/>
+        </td>
+        <td>
+            <input type="number" id="main-maint-price" name="main-maint-price" value="55"/>
+        </td>
+        <td>
+            <input type="number" id="reserve-maint-price" name="reserve-maint-price" value="75"/>
+        </td>
+        <td>
+            <input type="number" id="aad-maint-price" name="aad-maint-price" value="195"/>
+        </td>
+    </tr>
+</table>
+
+<table style="margin:0 auto;">
+    <tr>
+        <th>Hyppyjä päävarjolla</th>
+        <td>
+            <input type="number" id="main-jumps" name="main-jumps" value="0"/>
+        </td>
+    </tr>
+
+    <tr></tr>
+        <th>Päävarjon kesto (hyppyä)</th>
+        <td>
+            <input type="number" id="main-max-jumps" name="main-max-jumps" value="3000"/>
+        </td>
+    </tr>
+
+    <tr>
+        <th>Hyppyjä punoksilla</th>
+        <td>
+            <input type="number" id="lines-jumps" name="lines-jumps" value="0"/>
+        </td>
+    </tr>
+    
+    <tr>
+        <th>Punostenvaihdon hinta</th>
+        <td>
+            <input type="number" id="lines-change-price" name="lines-change-price" value="400"/>
+        </td>
+    </tr>
+    
+    <tr>
+        <th>Punosten kesto (hyppyä)</th>
+        <td>
+            <input type="number" id="lines-max-jumps" name="lines-max-jumps" value="700"/>
+        </td>
+    </tr>
+</table>
+</div>
+
+<table style="margin:0 auto;">
+    <tr>
+        <th>Valjaiden arvo</th>
+        <th>Päävarjon arvo</th>
+        <th>Varavarjon arvo</th>
+        <th>Cypressin arvo</th>
+        <th>Kokonaisarvo</th>
+    </tr>
+
+    <tr>
+        <td><input id="container-worth" value=0></div></td>
+        <td><input id="main-worth" value=0></div></td>
+        <td><input id="reserve-worth" value=0></div></td>
+        <td><input id="aad-worth"value=0></div></td>
+        <td><input id="total-worth"value=0></div></td>
+    </tr>
+</table>
+
+<script>
+    $("input").change(calculateWorth);
+    calculateWorth();
+</script>
+
---
+++ articles/kamalaskuri/kamalaskuri.js
@@ -0,0 +1,137 @@
+var calculateWorth = function() {
+    // Date related
+    var today = new Date();
+    var time_to_years = 1000*3600*24*365;
+
+    // Maintenance intervals
+    var interval_maint_main = 2;
+    var interval_maint_container = 2;
+    if (age_reserve > 14) {
+        interval_maint_container = 1;
+    }
+    var interval_maint_reserve = 1;
+    var interval_maint_aad = 4;
+
+    // Max ages
+    var max_age_main = 20;
+    var max_age_container = 20;
+    var max_age_reserve = 20;
+    var max_age_aad = 12;
+    
+    // Prices of components
+    var container = parseInt(($("#container-price").val()));
+    var main = parseInt($("#main-price").val());
+    var reserve = parseInt($("#reserve-price").val());
+    var aad = parseInt($("#aad-price").val());
+
+    // Manufacturing dates
+    var dom_container = new Date($("#container-dom").val());
+    var dom_main = new Date($("#main-dom").val());
+    var dom_reserve = new Date($("#reserve-dom").val());
+    var dom_aad = new Date($("#aad-dom").val());
+
+    // Ages of components
+    var age_container = Math.max(0,today.getTime() - dom_container.getTime())/time_to_years;
+    var age_main = Math.max(0,today.getTime() - dom_main.getTime())/time_to_years;
+    var age_reserve = Math.max(0,today.getTime() - dom_reserve.getTime())/time_to_years;
+    var age_aad = Math.max(0,today.getTime() - dom_aad.getTime())/time_to_years;
+
+    // Relative worth from age
+    var worth_container_relative = Math.max(0,Math.min(1,(max_age_container-age_container)/max_age_container));
+    var worth_main_relative = Math.max(0,Math.min(1,(max_age_main-age_main)/max_age_main));
+    var worth_reserve_relative = Math.max(0,Math.min(1,(max_age_reserve-age_reserve)/max_age_reserve));
+    var worth_aad_relative = Math.max(0,Math.min(1,(max_age_aad-age_aad)/max_age_aad));
+
+    // Next maintenance dates
+    var check_container = new Date($("#container-next-maint").val());
+    var check_main = new Date($("#main-next-maint").val());
+    var check_reserve = new Date($("#reserve-next-maint").val());
+    var check_aad = new Date($("#aad-next-maint").val());
+
+    // Years till maintenance
+    var years_to_maint_container = Math.max(0,check_container.getTime() - today.getTime())/time_to_years;
+    var years_to_maint_main = Math.max(0,check_main.getTime() - today.getTime())/time_to_years;
+    var years_to_maint_reserve = Math.max(0,check_reserve.getTime() - today.getTime())/time_to_years;
+    var years_to_maint_aad = Math.max(0,check_aad.getTime() - today.getTime())/time_to_years;
+
+    // Prices of maintenance
+    var maint_container = parseInt(($("#container-maint-price").val()));
+    var maint_main = parseInt(($("#main-maint-price").val()));
+    var maint_reserve = parseInt(($("#reserve-maint-price").val()));
+    var maint_aad = parseInt(($("#aad-maint-price").val()));
+
+    // Prices of lines and other
+    var lines = parseInt(($("#lines-change-price").val()));
+    var lines_max_jumps = parseInt(($("#lines-max-jumps").val()));
+    var lines_jumps = parseInt(($("#lines-jumps").val()));
+    var main_jumps = parseInt(($("#main-jumps").val()));
+    var main_jumps_max = parseInt(($("#main-max-jumps").val()));
+
+    // Prices reduction due to use
+    var lines_wear = 1.0-(lines_max_jumps-lines_jumps)/lines_max_jumps;
+    lines_wear = Math.max(0,Math.min(1,lines_wear));
+    // Final price of current lines
+    var worth_lines = lines*(1.0-lines_wear);
+    var main_wear = 1.0-(main_jumps_max-main_jumps)/main_jumps_max;
+    main_wear = Math.max(0,Math.min(1,main_wear));
+
+    // Prices of components if not maintained, takes into account the age
+    var container_without_extra = container - maint_container;
+    // Reduce price of new line set from the price of new main
+    var main_without_lines = main-lines;
+    // Reduce price reduction caused by wear from the main without lines
+    var main_without_wear = main_without_lines*(1.0-main_wear);
+    // Reduce price of maintenance
+    var main_without_extra = main_without_wear - maint_main;
+    var reserve_without_extra = reserve - maint_reserve;
+    var aad_without_extra = aad - maint_aad;
+
+    // Worth of current maintenance level
+    var worth_maint_container_relative = (1.0-(interval_maint_container-years_to_maint_container)/interval_maint_container);
+    worth_maint_container_relative = Math.min(1,Math.max(0,worth_maint_container_relative));
+    var worth_maint_container = worth_maint_container_relative*maint_container;
+
+    var worth_maint_main_relative = (1.0-(interval_maint_main-years_to_maint_main)/interval_maint_main);
+    worth_maint_main_relative = Math.min(1,Math.max(0,worth_maint_main_relative));
+    var worth_maint_main = worth_maint_main_relative*maint_main;
+
+    var worth_maint_reserve_relative = (1.0-(interval_maint_reserve-years_to_maint_reserve)/interval_maint_reserve);
+    worth_maint_reserve_relative = Math.min(1,Math.max(0,worth_maint_reserve_relative));
+    var worth_maint_reserve = worth_maint_reserve_relative*maint_reserve;
+
+    var worth_maint_aad_relative = (1.0-(interval_maint_aad-years_to_maint_aad)/interval_maint_aad);
+    worth_maint_aad_relative = Math.min(1,Math.max(0,worth_maint_aad_relative));
+    var worth_maint_aad = worth_maint_aad_relative*maint_aad;
+
+    // Calculate worth of components
+    var worth_main = Math.round( (main_without_extra + worth_lines + worth_maint_main) * worth_main_relative );
+    var worth_reserve = Math.round( (reserve_without_extra + worth_maint_reserve) * worth_reserve_relative);
+    var worth_container = Math.round( (container_without_extra + worth_maint_container) * worth_container_relative);
+    var worth_aad = Math.round( Math.max(100, (aad_without_extra + worth_maint_aad) * worth_aad_relative));
+
+    $("#container-worth").val(worth_container);
+    $("#aad-worth").val(worth_aad);
+    $("#main-worth").val(worth_main);
+    $("#reserve-worth").val(worth_reserve);
+    $("#total-worth").val(worth_main + worth_aad + worth_container + worth_reserve);
+    $("#total-price").val(container + main + reserve + aad);
+};
+
+$(document).ready(function() {
+    var today = new Date();
+    var day = ("0" + today.getDate()).slice(-2);
+    var month = ("0" + (today.getMonth() + 1)).slice(-2);
+    var year = today.getFullYear();
+
+    $("#container-dom").val(year + "-" + month + "-" + day);
+    $("#main-dom").val(year + "-" + month + "-" + day);
+    $("#reserve-dom").val(year + "-" + month + "-" + day);
+    $("#aad-dom").val(year + "-" + month + "-" + day);
+
+    $("#container-next-maint").val(year+2 + "-" + month + "-" + day);
+    $("#main-next-maint").val(year+2 + "-" + month + "-" + day);
+    $("#reserve-next-maint").val(year+2 + "-" + month + "-" + day);
+    $("#aad-next-maint").val(year+4 + "-" + month + "-" + day);
+
+    calculateWorth();
+});
